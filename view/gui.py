5c83e657b063c32655730ffcb40a763be684d400
ranguisheng <guisheng.ran@gmail.com>
2016-05-06 08:53:15 +0800

    gui.py

---
+++ view/gui.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from tkinter import  messagebox
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='Hello, world!')
+        self.helloLabel.pack()
+        
+        self.nameInput=Entry(self)
+        self.nameInput.pack()
+        
+        self.allertButton = Button(self,text='Hello',command=self.hello)
+        self.allertButton.pack()
+        
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
+    def hello(self):
+        name=self.nameInput.get() or 'world'
+        messagebox.showinfo('Message', 'Hello,%s' % name)
+app = Application()
+# 设置窗口标题:
+app.master.title('Hello World')
+# 主消息循环:
+app.mainloop()
